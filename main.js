907022d9dde41e55eec95e9081ff8a82e7d006ea
Alishba56 <164113933+Alishba56@users.noreply.github.com>
2024-05-11 23:26:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let Questions = await inquirer.prompt([
+        {
+            name: "Firstquestion",
+            type: "input",
+            message: "what would you like to add in you todos?",
+        },
+        {
+            name: "Secondquestion",
+            type: "confirm",
+            message: "would you like to add more in you todos?",
+            default: "true",
+        },
+    ]);
+    todos.push(Questions.Firstquestion);
+    condition = Questions.Secondquestion;
+    console.log(todos);
+}
